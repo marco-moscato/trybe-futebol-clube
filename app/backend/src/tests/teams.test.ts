0129867cb1bd04e4e9ef70ddc72dabb3fbf66413
Marco Moscato <marcomoscatto@gmail.com>
2023-07-13 18:47:03 -0300

    test<team.test>: testa a integração da rota GET /teams/:id

--- app/backend/src/tests/teams.test.ts
+++ app/backend/src/tests/teams.test.ts
@@ -3,7 +3,7 @@ import * as chai from 'chai';
 // @ts-ignore
 import chaiHttp = require('chai-http');
 import SequelizeTeam from '../database/models/SequelizeTeam';
-import { teams } from './mocks/teams/teams.mock';
+import { team, teams } from './mocks/teams/teams.mock';
 
 import { app } from '../app';
 
@@ -14,34 +14,11 @@ chai.use(chaiHttp);
 const { expect } = chai;
 
 describe('Dado a rota /teams', () => {
-  /**
-   * Exemplo do uso de stubs com tipos
-   */
 
-  // let chaiHttpResponse: Response;
+  afterEach(sinon.restore);
 
-  // before(async () => {
-  //   sinon
-  //     .stub(Example, "findOne")
-  //     .resolves({
-  //       ...<Seu mock>
-  //     } as Example);
-  // });
-
-  // after(()=>{
-  //   (Example.findOne as sinon.SinonStub).restore();
-  // })
-
-  // it('...', async () => {
-  //   chaiHttpResponse = await chai
-  //      .request(app)
-  //      ...
-
-  //   expect(...)
-  // });
-
-  describe('Dado a rota GET', () => {
-    it('Retorna o status 200 e um json contendo os times cadastrados', async function() {
+  describe('Dado o método GET', () => {
+    it('Retorna todos os times cadastradados com status 200', async function() {
       sinon.stub(SequelizeTeam, 'findAll').resolves(teams as any);
 
       const { status, body } = await chai.request(app).get('/teams');
@@ -49,6 +26,27 @@ describe('Dado a rota /teams', () => {
       expect(status).to.be.equal(200);
       expect(body).to.deep.equal(teams);
     });
+  
+    describe('Dado um ID que exista no banco', () => {
+      it('Retorna o time cadastrado com o status 200', async function() {
+        sinon.stub(SequelizeTeam, 'findByPk').resolves(team as any);
 
+        const { status, body } = await chai.request(app).get('/teams/1');
+        
+        expect(status).to.be.equal(200);
+        expect(body).to.deep.equal(team);
+      });
+    });
+
+    describe('Dado um ID inexistente', () => {
+      it('Retorna uma mensagem de erro com status 404', async function() {
+        sinon.stub(SequelizeTeam, 'findByPk').resolves(null);
+
+        const { status, body } = await chai.request(app).get('/teams/999');
+        
+        expect(status).to.be.equal(404);
+        expect(body.message).to.be.equal('Team 999 not found');
+      });
+    });
   });
 });
