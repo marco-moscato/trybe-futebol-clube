f6d6db96cf6cf8240bc71f51a160b908e295c881
Marco Moscato <marcomoscatto@gmail.com>
2023-07-13 17:24:11 -0300

    style<SequelizeTeam>: corrige erros de lint

---
+++ app/backend/src/database/models/SequelizeTeam.ts
@@ -0,0 +1,34 @@
+import {
+  DataTypes,
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+} from 'sequelize';
+import db from '.';
+
+class SequelizeTeam extends Model<InferAttributes<SequelizeTeam>,
+InferCreationAttributes<SequelizeTeam>> {
+  declare id: CreationOptional<number>;
+  declare teamName: string;
+}
+
+SequelizeTeam.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  teamName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  underscored: true,
+  modelName: 'teams',
+  timestamps: false,
+});
+
+export default SequelizeTeam;
