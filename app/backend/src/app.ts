82502d49210c9f046327b848523377a24c415bc4
Marco Moscato <marcomoscatto@gmail.com>
2023-07-13 14:21:58 -0300

    Iniciando o projeto...

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -30,6 +30,6 @@ class App {
 }
 
 export { App };
-
+// Iniciando o projeto...
 // Essa segunda exportação é estratégica, e a execução dos testes de cobertura depende dela
 export const { app } = new App();
