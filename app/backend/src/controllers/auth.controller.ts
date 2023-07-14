288067439cc0f62dfaf2ffc1e31ee0ca80f3c5bf
Marco Moscato <marcomoscatto@gmail.com>
2023-07-14 11:24:16 -0300

    refactor<auth.controller>: valida se o email informada está cadastrado

--- app/backend/src/controllers/auth.controller.ts
+++ app/backend/src/controllers/auth.controller.ts
@@ -1,10 +1,18 @@
 import { Request, Response } from 'express';
 import * as jwt from 'jsonwebtoken';
+import UserModel from '../models/user.model';
 
 class AuthController {
   jwt = jwt.sign;
+  userModel = new UserModel();
+
   async login(req: Request, res: Response) {
-    const token = this.jwt({ id: 1 }, 'secret');
+    const { email } = req.body;
+    const user = await this.userModel.findByEmail(email);
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    const token = this.jwt({ id: user.id }, 'secret');
     return res.status(200).json({ token });
   }
 }
