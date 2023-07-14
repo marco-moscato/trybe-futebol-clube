b99468650f599b829ea00b0ffdc22c9d54b338f1
Marco Moscato <marcomoscatto@gmail.com>
2023-07-14 11:27:00 -0300

    feat<user.model>: cria função que busca usuário por email

--- app/backend/src/models/user.model.ts
+++ app/backend/src/models/user.model.ts
@@ -1,9 +1,14 @@
-// import { IUserModel } from '../Interfaces/users/IUserModel';
-// import SequelizeUser from '../database/models/SequelizeUser';
+import { IUser } from '../Interfaces/users/IUser';
+import { IUserModel } from '../Interfaces/users/IUserModel';
+import SequelizeUser from '../database/models/SequelizeUser';
 
-// export default class UserModel implements IUserModel {
-//   private model = SequelizeUser;
+class UserModel implements IUserModel {
+  private model = SequelizeUser;
 
-//   async findOne()
+  async findByEmail(email: IUser['email']): Promise<IUser | null> {
+    const user = await this.model.findOne({ where: { email } });
+    return !user ? null : user;
+  }
+}
 
-// }
+export default UserModel;
