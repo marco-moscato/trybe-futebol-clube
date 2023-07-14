46da02a8df26773d0699c1c9d2b9515ed0cfec76
Marco Moscato <marcomoscatto@gmail.com>
2023-07-14 15:47:24 -0300

    test<user>: testa validações de login, jwt e token

--- app/backend/src/tests/mocks/users/users.mock.ts
+++ app/backend/src/tests/mocks/users/users.mock.ts
@@ -4,8 +4,8 @@ export const userMock: IUser = {
   id: 1,
   username: 'fakeuser',
   role: 'admin',
-  email: 'fakeuser@fakeemail.com',
-  password: '123456',
+  email: 'valid@email.com',
+  password: 'valid-password',
 }
 
 export const tokenMock = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwicm9sZSI6ImFkbWluIiwiaWF0IjoxNjU0NTI3MTg5fQ.XS_9AA82iNoiVaASi0NtJpqOQ_gHSHhxrpIdigiT-fc";
