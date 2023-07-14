25ba3ecadb1b7d5bb9a3592877c098741bba8900
Marco Moscato <marcomoscatto@gmail.com>
2023-07-14 15:46:51 -0300

    reafactor: inclui resposta unauthorized em mapStatusHTTP

--- app/backend/src/utils/mapStatusHTTP.ts
+++ app/backend/src/utils/mapStatusHTTP.ts
@@ -4,6 +4,7 @@ export default function mapStatusHTTP(status: string): number {
     case 'INVALID_DATA': return 400;
     case 'NOT_FOUND': return 404;
     case 'CONFLICT': return 409;
+    case 'UNAUTHORIZED': return 401;
     default: return 500;
   }
 }
