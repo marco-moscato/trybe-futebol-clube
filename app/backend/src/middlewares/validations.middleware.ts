390b39543a83bbbdc18ad0da46ef10a98732546d
Marco Moscato <marcomoscatto@gmail.com>
2023-07-14 15:44:36 -0300

    refactor<validation>: cria validação de formato de email e senha

--- app/backend/src/middlewares/validations.middleware.ts
+++ app/backend/src/middlewares/validations.middleware.ts
@@ -1,13 +1,20 @@
 import { NextFunction, Request, Response } from 'express';
+import { ILogin } from '../Interfaces/users/IUser';
 
 class Validations {
   static validateLogin(req: Request, res: Response, next: NextFunction) {
-    const { email, password } = req.body;
+    const { email, password } = req.body as ILogin;
 
     if (!email || !password) {
       return res.status(400).json({
         message: 'All fields must be filled',
       });
+    }
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!(emailRegex.test(email)) || password.length < 6) {
+      return res.status(401).json({
+        message: 'Invalid email or password',
+      })
     }
     next();
   }
