71603fdab90c4aeb6635165ebc6ac964a7a2044f
Marco Moscato <marcomoscatto@gmail.com>
2023-07-14 15:45:09 -0300

    feat<user.service>: cria camada service de usuário com validações
    - valida com bcrypt e jwt

---
+++ app/backend/src/services/user.service.ts
@@ -0,0 +1,39 @@
+import { ServiceResponse } from "../Interfaces/ServiceResponse";
+import { ILogin } from "../Interfaces/users/IUser";
+import UserModel from "../models/user.model";
+import Jwt from "../utils/jwtAuth";
+import * as bcrypt from 'bcryptjs';
+
+class UserService {
+
+  private userModel = new UserModel();
+  private jwt = new Jwt();
+
+  async findByEmail(login: ILogin): Promise<ServiceResponse<{ token: string }>> {
+    const { email, password } = login;
+    const user = await this.userModel.findByEmail(email);
+
+    if (!user) {
+      return {
+        status: 'UNAUTHORIZED',
+        data: { message: 'Invalid email or password' },
+      };
+    }
+
+    if (!bcrypt.compareSync(password, user.password)) {
+      return {
+        status: 'UNAUTHORIZED',
+        data: { message: 'Invalid email or password' }
+      }
+    }
+
+    const token = this.jwt.sign({ id: user.id });
+
+    return {
+      status: 'SUCCESSFUL',
+      data: { token },
+    }
+  }
+}
+
+export default UserService;
