3a2d2407f2b88a8c27e525535c0626d3654a57af
Marco Moscato <marcomoscatto@gmail.com>
2023-07-17 17:58:34 -0300

    feat: cria a rota /login/role em auth.routes

--- app/backend/src/routes/auth.routes.ts
+++ app/backend/src/routes/auth.routes.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 import AuthController from '../controllers/auth.controller';
 import Validations from '../middlewares/validations.middleware';
+import Auth from '../middlewares/auth.middleware';
+import UserModel from '../models/user.model';
 
 const authController = new AuthController();
+const userModel = new UserModel();
 
 const router = Router();
 
@@ -10,6 +13,15 @@ router.post(
   '/',
   Validations.validateLogin,
   (req, res) => authController.login(req, res),
+);
+
+router.get(
+  '/role',
+  Auth.verifyToken,
+  async (req, res) => {
+    const userRole = res.locals.user.role;
+    return res.status(200).json({ role: userRole });
+  }
 );
 
 export default router;
