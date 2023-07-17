5b1f290f9640f612700287532addacad23e942f7
Marco Moscato <marcomoscatto@gmail.com>
2023-07-17 18:00:58 -0300

    feat<jwtAuth>: cria função verify

--- app/backend/src/utils/jwtAuth.ts
+++ app/backend/src/utils/jwtAuth.ts
@@ -1,11 +1,17 @@
 import * as jwt from 'jsonwebtoken';
 import { Identifiable } from '../Interfaces';
+import { ITokenCreator } from '../Interfaces/ITokenCreator';
 
-class Jwt {
+class Jwt implements ITokenCreator {
   jwtSecret = process.env.JWT_SECRET || 'secret';
 
-  sign(payload: Identifiable): string {
+  public sign(payload: Identifiable): string {
     return jwt.sign(payload, this.jwtSecret);
+  }
+
+  public verify(token: string): jwt.JwtPayload {
+    const decoded = jwt.verify(token, this.jwtSecret);
+    return decoded as jwt.JwtPayload;
   }
 }
 
