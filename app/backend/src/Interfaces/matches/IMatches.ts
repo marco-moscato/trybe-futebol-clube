bc9a3ec9b2c6f605eba921a111e5a556b76255c7
Marco Moscato <marcomoscatto@gmail.com>
2023-07-17 19:54:16 -0300

    feat: cria migration e seeders para tabela matches

--- app/backend/src/Interfaces/matches/IMatches.ts
+++
@@ -1,8 +0,0 @@
-export interface IMatch {
-  id: number,
-  home_team_id: number,
-  home_team_goals: number,
-  away_team_id: number,
-  away_team_goals: number,
-  in_progress: boolean,
-}
---
+++ app/backend/src/database/migrations/03-create-matches-table.ts
@@ -0,0 +1,51 @@
+import { Model, QueryInterface, DataTypes } from 'sequelize';
+import { IMatch } from '../../Interfaces/matches/IMatch';
+
+export default {
+  up(queryInterface: QueryInterface) {
+    return queryInterface.createTable<Model<IMatch>>('matches', {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      homeTeamId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: "teams",
+          key: "id",
+        },
+        field: 'home_team_id',
+      },
+      homeTeamGoals: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        field: 'home_team_goals',
+      },
+      awayTeamId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: "teams",
+          key: "id",
+        },
+        field: 'away_team_id',
+      },
+      awayTeamGoals: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        field: 'away_team_goals',
+      },
+      inProgress: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        field: 'in_progress',
+      },
+    });
+  },
+  down(queryInterface: QueryInterface) {
+    return queryInterface.dropTable('matches');
+  },
+};
