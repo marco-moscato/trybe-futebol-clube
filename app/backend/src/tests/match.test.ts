47ff6eb7f76b9a38027d6a2467be7fa285577cca
Marco Moscato <marcomoscatto@gmail.com>
2023-07-18 14:52:20 -0300

    refactor<match.test>: refatora testes que busca partidas por status

--- app/backend/src/tests/match.test.ts
+++ app/backend/src/tests/match.test.ts
@@ -5,8 +5,7 @@ import chaiHttp = require('chai-http');
 import SequelizeMatch from '../database/models/SequelizeMatch';
 
 import { app } from '../app';
-import { fakeMatches } from './mocks/matches/match.mock';
-import SequelizeTeam from '../database/models/SequelizeTeam';
+import { allMatches } from './mocks/matches/match.mock';
 
 chai.use(chaiHttp);
 
@@ -17,60 +16,39 @@ describe('Dado a rota /matches', () => {
   afterEach(sinon.restore);
 
   describe('Dado o método GET', () => {
-    it('Retorna todos as partidas cadastradados com status 200', async function() {
-      const matches = SequelizeMatch.bulkBuild(fakeMatches, {
-        include: [
-          { model: SequelizeTeam, as: 'homeTeam', attributes: { exclude: ['id'] } },
-          { model: SequelizeTeam, as: 'awayTeam', attributes: { exclude: ['id'] } },
-        ],
-      });
-
-      sinon.stub(SequelizeMatch, 'findAll').resolves(matches);
+    it('Retorna todos as partidas cadastradados com status 200', async function() {         
+      sinon.stub(SequelizeMatch, 'findAll').resolves(allMatches as any);
 
       const { status, body } = await chai.request(app).get('/matches');
       
       expect(status).to.be.equal(200);
-      expect(body).to.deep.equal(fakeMatches);
+      expect(body).to.deep.equal(allMatches);
     });
 
     it('Retorna as partidas em andamento com status 200', async function() {
-      const matches = SequelizeMatch.bulkBuild(fakeMatches, {
-        include: [
-          { model: SequelizeTeam, as: 'homeTeam', attributes: { exclude: ['id'] } },
-          { model: SequelizeTeam, as: 'awayTeam', attributes: { exclude: ['id'] } },
-        ],
-      });
+      sinon.stub(SequelizeMatch, 'findAll').resolves(allMatches as any);
 
-      sinon.stub(SequelizeMatch, 'findAll').resolves(matches);
-      
-      const matchesInProgress = matches.filter((match) => match.inProgress === true)
+      const filteredMatches = allMatches.filter((match) => match.inProgress === true)
       
       const { status, body } = await chai.request(app)
       .get('/matches')
       .query('inProgress=true');
       
       expect(status).to.be.equal(200);
-      expect(body).to.deep.equal(matchesInProgress);
+      expect(body).to.deep.equal(filteredMatches);
     });
 
     it('Retorna as partidas finalizadas com status 200', async function() {
-      const matches = SequelizeMatch.bulkBuild(fakeMatches, {
-        include: [
-          { model: SequelizeTeam, as: 'homeTeam', attributes: { exclude: ['id'] } },
-          { model: SequelizeTeam, as: 'awayTeam', attributes: { exclude: ['id'] } },
-        ],
-      });
-
-      sinon.stub(SequelizeMatch, 'findAll').resolves(matches);
+      sinon.stub(SequelizeMatch, 'findAll').resolves(allMatches as any);
       
-      const matchesInProgress = matches.filter((match) => match.inProgress === false)
+      const filteredMatches = allMatches.filter((match) => match.inProgress === false)
       
       const { status, body } = await chai.request(app)
       .get('/matches')
       .query('inProgress=false');
       
       expect(status).to.be.equal(200);
-      expect(body).to.deep.equal(matchesInProgress);
+      expect(body).to.deep.equal(filteredMatches);
     });
   });
 });
--- app/backend/src/tests/mocks/matches/match.mock.ts
+++ app/backend/src/tests/mocks/matches/match.mock.ts
@@ -1,12 +1,37 @@
 import { IMatch } from '../../../Interfaces/matches/IMatch';
 
-export const fakeMatch: IMatch = {
+export const finishedMatch = {
   id: 1,
   homeTeamId: 2,
   homeTeamGoals: 2,
   awayTeamId: 3,
   awayTeamGoals: 3,
   inProgress: false,
-}
+  homeTeam: {
+    teamName: 'Fake Name A'
+  },
+  awayTeam: {
+    teamName: 'Fake Name B'
+  }
+};
 
-export const fakeMatches = [fakeMatch];
+export const matchInProgress = {
+  id: 1,
+  homeTeamId: 2,
+  homeTeamGoals: 2,
+  awayTeamId: 3,
+  awayTeamGoals: 3,
+  inProgress: true,
+  homeTeam: {
+    teamName: 'Fake Name A'
+  },
+  awayTeam: {
+    teamName: 'Fake Name B'
+  }
+};
+
+export const allMatches = [matchInProgress, finishedMatch];
+
+export const matchesInProgress = [matchInProgress];
+
+export const finishedMatches = [finishedMatch];
