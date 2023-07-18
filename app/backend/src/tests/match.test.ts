6ad91b261bded07120863ab162669819b8b74987
Marco Moscato <marcomoscatto@gmail.com>
2023-07-18 11:46:57 -0300

    test<match.test>: testa função de filtrar partidas por status

--- app/backend/src/tests/match.test.ts
+++ app/backend/src/tests/match.test.ts
@@ -32,5 +32,45 @@ describe('Dado a rota /matches', () => {
       expect(status).to.be.equal(200);
       expect(body).to.deep.equal(fakeMatches);
     });
+
+    it('Retorna as partidas em andamento com status 200', async function() {
+      const matches = SequelizeMatch.bulkBuild(fakeMatches, {
+        include: [
+          { model: SequelizeTeam, as: 'homeTeam', attributes: { exclude: ['id'] } },
+          { model: SequelizeTeam, as: 'awayTeam', attributes: { exclude: ['id'] } },
+        ],
+      });
+
+      sinon.stub(SequelizeMatch, 'findAll').resolves(matches);
+      
+      const matchesInProgress = matches.filter((match) => match.inProgress === true)
+      
+      const { status, body } = await chai.request(app)
+      .get('/matches')
+      .query('inProgress=true');
+      
+      expect(status).to.be.equal(200);
+      expect(body).to.deep.equal(matchesInProgress);
+    });
+
+    it('Retorna as partidas finalizadas com status 200', async function() {
+      const matches = SequelizeMatch.bulkBuild(fakeMatches, {
+        include: [
+          { model: SequelizeTeam, as: 'homeTeam', attributes: { exclude: ['id'] } },
+          { model: SequelizeTeam, as: 'awayTeam', attributes: { exclude: ['id'] } },
+        ],
+      });
+
+      sinon.stub(SequelizeMatch, 'findAll').resolves(matches);
+      
+      const matchesInProgress = matches.filter((match) => match.inProgress === false)
+      
+      const { status, body } = await chai.request(app)
+      .get('/matches')
+      .query('inProgress=false');
+      
+      expect(status).to.be.equal(200);
+      expect(body).to.deep.equal(matchesInProgress);
+    });
   });
 });
