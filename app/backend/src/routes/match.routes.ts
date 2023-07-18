8fa06eb60739d0e9d644b44410031697788a3977
Marco Moscato <marcomoscatto@gmail.com>
2023-07-18 16:32:03 -0300

    feat<match.routes>: cria função para finalizar uma partida

--- app/backend/src/routes/match.routes.ts
+++ app/backend/src/routes/match.routes.ts
@@ -2,6 +2,8 @@ import { Request, Response, Router } from 'express';
 import MatchController from '../controllers/match.controller';
 import MatchService from '../services/match.service';
 import MatchModel from '../models/match.model';
+import Auth from '../middlewares/auth.middleware';
+import SequelizeMatch from '../database/models/SequelizeMatch';
 
 const router = Router();
 const matchModel = new MatchModel();
@@ -10,7 +12,29 @@ const matchController = new MatchController(matchService);
 
 router.get(
   '/',
-  async (_req: Request, res: Response) => matchController.findAll(_req, res),
+  (_req: Request, res: Response) => matchController.findAll(_req, res),
+);
+router.patch(
+  '/:id/finish',
+  Auth.verifyToken,
+  async (req: Request, res: Response) => {
+    const id = Number(req.params.id);
+
+    const [affectedRows] = await SequelizeMatch.update(
+      { inProgress: false },
+      { where: { id } },
+    );
+
+    if (affectedRows === 0) {
+      return res.status(400).json({
+        message: 'No match has been finished',
+      });
+    }
+
+    if (affectedRows > 0) {
+      return res.status(200).json({ message: 'Finished' });
+    }
+  },
 );
 
 export default router;
