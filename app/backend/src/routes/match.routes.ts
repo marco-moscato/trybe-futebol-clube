25d5b6f543bc9294b5c20a6789c325cedef04ab1
Marco Moscato <marcomoscatto@gmail.com>
2023-07-18 19:32:48 -0300

    feat<match.routes>: cria o endpoint PATCH /matches/:id que atualiza uma partida

--- app/backend/src/routes/match.routes.ts
+++ app/backend/src/routes/match.routes.ts
@@ -3,6 +3,8 @@ import MatchController from '../controllers/match.controller';
 import MatchService from '../services/match.service';
 import MatchModel from '../models/match.model';
 import Auth from '../middlewares/auth.middleware';
+import SequelizeMatch from '../database/models/SequelizeMatch';
+import { IMatch } from '../Interfaces/matches/IMatch';
 
 const router = Router();
 const matchModel = new MatchModel();
@@ -17,6 +19,22 @@ router.patch(
   '/:id/finish',
   Auth.verifyToken,
   (req, res) => matchController.finish(req, res),
+);
+router.patch(
+  '/:id/',
+  Auth.verifyToken,
+  async (req, res) => {
+    const id = Number(req.params.id);
+    const infoToUpdate: Partial<IMatch> = req.body;
+    const [affectedRows] = await SequelizeMatch.update(
+      infoToUpdate,
+      { where: { id } },
+    );
+    if (affectedRows === 0) {
+      return res.status(400).json({ message: 'No match has been updated' });
+    }
+    return res.status(200).json({ message: 'Match sucessfully updated' });
+  },
 );
 
 export default router;
