5f3da17b4783fdce34b83cc4d680750e3038563c
Marco Moscato <marcomoscatto@gmail.com>
2023-07-18 11:47:24 -0300

    feat<match.controller>: cria função de filtrar partidas por status

--- app/backend/src/controllers/match.controller.ts
+++ app/backend/src/controllers/match.controller.ts
@@ -6,9 +6,16 @@ class MatchController {
     private matchService = new MatchService(),
   ) {}
 
-  public async findAll(_req: Request, res: Response) {
-    const serviceResponse = await this.matchService.findAll();
-    res.status(200).json(serviceResponse.data);
+  public async findAll(req: Request, res: Response) {
+    const { data } = await this.matchService.findAll();
+
+    const matchStatus = req.query.inProgress;
+
+    if (Array.isArray(data) && matchStatus) {
+      const filteredMatches = data.filter((match) => match.inProgress.toString() === matchStatus);
+      return res.status(200).json(filteredMatches);
+    }
+    return res.status(200).json(data);
   }
 }
 
