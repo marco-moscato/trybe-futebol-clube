909d53df463b022fb960300d7321885232725d8d
Marco Moscato <marcomoscatto@gmail.com>
2023-07-18 10:34:32 -0300

    feat<routes/index>: cria rota /matches

--- app/backend/src/routes/index.ts
+++ app/backend/src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import teamsRouter from './teams.routes';
 import authRouter from './auth.routes';
+import matchRouter from './match.routes';
 
 const router = Router();
 
 router.use('/teams', teamsRouter);
 router.use('/login', authRouter);
+router.use('/matches', matchRouter)
 
 export default router;
