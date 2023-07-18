db87597c912e7cf1141497b71526e36a24d3be06
Marco Moscato <marcomoscatto@gmail.com>
2023-07-18 16:32:25 -0300

    test<match.test>: testa função que finaliza uma partida

--- app/backend/src/tests/match.test.ts
+++ app/backend/src/tests/match.test.ts
@@ -6,6 +6,8 @@ import SequelizeMatch from '../database/models/SequelizeMatch';
 
 import { app } from '../app';
 import { allMatches } from './mocks/matches/match.mock';
+import Jwt from '../utils/jwtAuth';
+import { userMock } from './mocks/users/users.mock';
 
 chai.use(chaiHttp);
 
@@ -49,6 +51,81 @@ describe('Dado a rota /matches', () => {
       
       expect(status).to.be.equal(200);
       expect(body).to.deep.equal(filteredMatches);
+    });
+  });
+
+  describe('Dado o método PATCH, tenta finalizar uma partida', () => {
+    describe('Dado que um token não seja informado', () => {
+      it('Retorna uma mensagem de erro com status 401', async function() {         
+        const { status, body } = await chai.request(app).patch('/matches/1/finish');
+      
+        expect(status).to.be.equal(401);
+        expect(body.message).to.deep.equal('Token not found');
+      });
+
+    describe('Dado um token inválido', () => {
+      it('Retorna um erro com status 401', async function() {         
+        const invalidToken = 'invalidToken';
+
+        const { status, body } = await chai.request(app)
+        .patch('/matches/1/finish')
+        .set({ Authorization: `Bearer ${invalidToken}` });
+      
+        expect(status).to.be.equal(401);
+        expect(body.message).to.deep.equal('Token must be a valid token');
+      });
+    });
+  });
+
+    describe('Dado um token válido', () => {
+      describe('Dado o id de uma partida que não existe', () => {
+        it('Retorna uma mensagem de erro com o status 400', async function() {         
+          const validToken = 'validToken';
+          sinon.stub(SequelizeMatch, 'update').resolves([0]);
+          sinon.stub(Jwt.prototype, 'verify').returns(userMock);
+
+          const { status, body } = await chai.request(app)
+          .patch('/matches/999/finish')
+          .set({ Authorization: `Bearer ${validToken}` });
+        
+          expect(status).to.be.equal(400);
+          expect(body.message).to.deep.equal('No match has been finished');
+        });
+      });
+    });
+
+    describe('Dado um token válido', () => {
+      describe('Dado um id de uma partida já finalizada no banco', () => {
+        it('Retorna uma mensagem de erro com o status 400', async function() {         
+          const validToken = 'validToken';
+          sinon.stub(SequelizeMatch, 'update').resolves([0]);
+          sinon.stub(Jwt.prototype, 'verify').returns(userMock);
+
+          const { status, body } = await chai.request(app)
+          .patch('/matches/1/finish')
+          .set({ Authorization: `Bearer ${validToken}` });
+        
+          expect(status).to.be.equal(400);
+          expect(body.message).to.deep.equal('No match has been finished');
+        });
+      });
+    });
+
+    describe('Dado um token válido', () => {
+      describe('Dado um id de uma partida que exista no banco e está e andamento', () => {
+        it('Retorna uma mensagem de sucesso com o status 200', async function() {         
+          const validToken = 'validToken';
+          sinon.stub(SequelizeMatch, 'update').resolves([1]);
+          sinon.stub(Jwt.prototype, 'verify').returns(userMock);
+
+          const { status, body } = await chai.request(app)
+          .patch('/matches/1/finish')
+          .set({ Authorization: `Bearer ${validToken}` });
+        
+          expect(status).to.be.equal(200);
+          expect(body.message).to.deep.equal('Finished');
+        });
+      });
     });
   });
 });
