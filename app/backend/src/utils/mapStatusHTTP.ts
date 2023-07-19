de17b6a65c6a40c51cbc3cacb4c5d8c8466bb1f4
Marco Moscato <marcomoscatto@gmail.com>
2023-07-19 11:38:23 -0300

    refactor<mapStatusHTTP>: inclui caso 'UNPROCESSABLE'

--- app/backend/src/utils/mapStatusHTTP.ts
+++ app/backend/src/utils/mapStatusHTTP.ts
@@ -5,6 +5,7 @@ export default function mapStatusHTTP(status: string): number {
     case 'NOT_FOUND': return 404;
     case 'CONFLICT': return 409;
     case 'UNAUTHORIZED': return 401;
+    case 'UNPROCESSABLE': return 422;
     default: return 500;
   }
 }
